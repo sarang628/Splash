f073594948ebbc6ef5d30aa60de85483f4b691ff
SRYANG <sarang628@naver.com>
2023-10-26 12:21:30 +0800

    update

--- app/src/main/java/com/sryang/splash/MainActivity.kt
+++ app/src/main/java/com/sryang/splash/MainActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.Modifier
 import com.sarang.screen_splash.compose.SplashScreen
-import com.sryang.splash.ui.theme.SplashTheme
 import com.sryang.torang_repository.repository.LoginRepository
 import com.sryang.torang_repository.repository.LoginRepositoryTest
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -25,12 +21,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplashTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            Column {
+                Column(Modifier.weight(1f)) {
                     SplashScreen(onLoginExpired = {
                         Log.d("MainActivity", "onLoginExpired")
                     }, onSuccess = {
@@ -38,7 +30,8 @@ class MainActivity : ComponentActivity() {
                     }, unLogin = {
                         Log.d("MainActivity", "unLogin")
                     })
-
+                }
+                Column(Modifier.weight(1f)) {
                     LoginRepositoryTest(loginRepository = loginRepository)
                 }
             }
--- library/src/main/java/com/sarang/screen_splash/compose/SplashScreen.kt
+++ library/src/main/java/com/sarang/screen_splash/compose/SplashScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Button
@@ -84,7 +85,7 @@ fun SplashScreen(
                         Text(text = "확인")
                     }
                 },
-                title = { Text(text = "Your login expired.") }
+                title = { Text(text = "Your login expired.", fontSize = 16.sp) }
             )
         } else if (uiState.isLogin == false) {
             unLogin.invoke()
