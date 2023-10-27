f8e255a7c26dd18cab3cc211f16e09ba744294e0
SRYANG <sarang628@naver.com>
2023-10-27 14:14:10 +0800

    update

---
+++ library/src/main/java/com/sryang/splash/compose/SplashScreen.kt
@@ -0,0 +1,97 @@
+package com.sryang.splash.compose
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.sryang.splash.uistate.LoginState
+import com.sryang.splash.viewmodel.SplashViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun SplashScreen(
+    splashViewModel: SplashViewModel = hiltViewModel(),
+    onLoginState: (LoginState?) -> Unit
+) {
+
+    val uiState by splashViewModel.uiState.collectAsState()
+    val coroutine = rememberCoroutineScope()
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(colorResource(id = com.sarang.theme.R.color.colorSecondaryLight))
+    ) {
+
+        Spacer(modifier = Modifier.height(100.dp))
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "T O R A N G",
+                color = colorResource(id = com.sarang.theme.R.color.colorSecondary),
+                fontSize = 45.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Hit the spot",
+                color = colorResource(id = com.sarang.theme.R.color.colorSecondary),
+                fontSize = 20.sp
+            )
+        }
+
+        //세션 만료일 경우 자체적으로 팝업 발생
+        if (uiState.loginState == LoginState.SESSION_EXPIRED) {
+            AlertDialog(
+                onDismissRequest = { },
+                confirmButton = {
+                    Button(onClick = {
+                        coroutine.launch {
+                            splashViewModel.logout()
+                        }
+                    }) {
+                        Text(text = "확인")
+                    }
+                },
+                title = { Text(text = "Your login expired.", fontSize = 16.sp) }
+            )
+        } else { //로그인이나 로그아웃 상태일 경우 콜백
+            onLoginState.invoke(uiState.loginState)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSplashScreen() {
+    val context = LocalContext.current
+    SplashScreen(onLoginState = {
+
+    })
+}
---
+++ library/src/main/java/com/sryang/splash/viewmodel/SplashViewModel.kt
@@ -0,0 +1,124 @@
+package com.sryang.splash.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sryang.splash.uistate.LoginState
+import com.sryang.splash.uistate.SplashUiState
+import com.sryang.splash.usecase.SplashUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * 스플레시 뷰모델
+ */
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val splashService: SplashUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SplashUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        //로그인 상태 체크
+        viewModelScope.launch {
+            // 토큰이 없다면 로그아웃 상태
+            if (!splashService.isLogin()) {
+                _uiState.emit(
+                    uiState.value.copy(
+                        loginState = LoginState.LOGOUT
+                    )
+                )
+            } else if (!splashService.checkSession()) { //세션체크
+                _uiState.emit(
+                    uiState.value.copy(
+                        loginState = LoginState.SESSION_EXPIRED
+                    )
+                )
+            } else { // 로그인
+                _uiState.emit(
+                    uiState.value.copy(
+                        loginState = LoginState.LOGIN
+                    )
+                )
+            }
+        }
+    }
+
+    fun logout() {
+        viewModelScope.launch {
+            splashService.logout()
+            _uiState.emit(
+                uiState.value.copy(
+                    loginState = LoginState.LOGOUT
+                )
+            )
+        }
+    }
+
+    // 프로그레스 점
+    @JvmField
+    var progress = MutableLiveData<String>()
+
+    // 준비완료 상태
+    val isReady = MutableLiveData<Boolean>()
+
+    // 네비게이션 인터페이스
+
+    // 장비 등록 여부
+    val isRegisteredDevice = MutableLiveData<Boolean>()
+
+    // 위변조 체크 여부
+    val isForgery = MutableLiveData<Boolean>()
+
+    // 에러메시지
+    private val _errorMsg = MutableLiveData<String>()
+
+    @JvmField
+    var errorMsg: LiveData<String> = _errorMsg
+
+    // 장비 관지라
+
+
+    // 저장소
+
+
+    // 프로그레스바 런어블
+    private var runnable: Runnable? = null
+    fun setIsReady(isReady: Boolean) {
+        this.isReady.value = isReady
+    }
+
+    /**
+     * 프로그레스바 시작
+     */
+    fun startProgress() {
+    }
+
+    /**
+     * 프로그레스바 멈춤
+     */
+    fun stopProgress() {
+    }
+
+    /**
+     * 장비등록
+     */
+    fun registerDevice() {
+
+    }
+
+    /**
+     * 생성자
+     *
+     * @param context
+     */
+    init {
+        progress.value = ""
+    }
+}
